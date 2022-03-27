6f2973d098daeee40902379754e1cb27f9202345
Matteo Calderaro <matteocalderaro@gmail.com>
2022-03-27 15:02:30 +0200

    Add auth endpoints with Sanctum

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Tweet;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
@@ -15,6 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        User::factory()->create([
+            'name'=> 'michele',
+            'email'=> 'm@m',
+        ]);
+
         Tweet::factory(40)->create();
     }
 }
